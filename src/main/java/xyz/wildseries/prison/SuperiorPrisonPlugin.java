6a14fc77fdc464b700deeebda89d83a26415c809
BuildTools <unconfigured@null.spigotmc.org>
2018-12-26 18:34:34 +0200

    Initial Commit
    Created the Project
    Added Main Classes
    Added plugin.yml

---
+++ src/main/java/xyz/wildseries/prison/SuperiorPrisonPlugin.java
@@ -0,0 +1,22 @@
+package xyz.wildseries.prison;
+
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+@Getter
+public class SuperiorPrisonPlugin extends JavaPlugin {
+
+    @Getter private static SuperiorPrisonPlugin plugin;
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+    }
+
+    public static void log(String message){
+        plugin.getLogger().log(Level.INFO, message);
+    }
+
+}
